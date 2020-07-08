f6a692cf8469098178e0309aa81c71cbd306d504
wilson <yxw_android@163.com>
2020-07-08 13:55:25 +0800

    init

--- app/src/main/java/com/example/aidltest/ClientActivity.java
+++ app/src/main/java/com/example/aidltest/ClientActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.aidltest.myaidl.IUserManager;
 import com.example.aidltest.myaidl.Stub;
